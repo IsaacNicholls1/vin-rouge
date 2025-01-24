2d4b9bf42223ae88343ea3ca57dc7bb0fb3a3e1d
Isaac Nicholls <isaac.nicholls1@gmail.com>
2025-01-24 16:53:22 +0000

    link up south_west

--- vin-rouge/urls.py
+++ vin-rouge/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
-    path('south-west/', views.south_west, name='south_west'),
+    path('blog/south_west', include('blog.urls'))
     
 ]
