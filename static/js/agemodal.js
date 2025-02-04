1d9bbd3382d970dde7ec5e4b09bf00e462e2e216
Isaac Nicholls <isaac.nicholls1@gmail.com>
2025-02-04 13:40:23 +0000

    add age verification modal

---
+++ static/js/agemodal.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (!localStorage.getItem('ageVerified')) {
+        var ageVerificationModal = new bootstrap.Modal(document.getElementById('ageVerificationModal'));
+        ageVerificationModal.show();
+
+        document.getElementById('confirmAge').addEventListener('click', function () {
+            localStorage.setItem('ageVerified', 'true');
+            ageVerificationModal.hide();
+        });
+    }
+});
