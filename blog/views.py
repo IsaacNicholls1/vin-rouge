d1fd09bd49a47bb3852d2e9dde5ab86668b5745a
Isaac Nicholls <isaac.nicholls1@gmail.com>
2025-02-07 14:30:04 +0000

    add comments

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,24 @@
-from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.contrib import messages
-from django.http import HttpResponseRedirect
-from .models import Review, Comment, Wine
-from .forms import CommentForm, ReviewForm
-from django.views.generic import View
+from .models import Comment, Wine
 from django.views.generic.edit import CreateView, UpdateView
-from django.utils.text import slugify
-from django.utils import timezone
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 
-#index view
+# 'index view' is the home page view.
+
+
 def index(request):
     return render(request, 'blog/index.html')
+
 
 class WineList(generic.ListView):
     model = Wine
     template_name = "blog/wine_list.html"
     context_object_name = "wines"
     paginate_by = 6
+
 
 class WineDetail(generic.DetailView):
     model = Wine
@@ -28,43 +27,43 @@ class WineDetail(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.filter(wine=self.object)
+        comments = Comment.objects.filter(wine=self.object)
+        context['comments'] = comments
+        context['comment_count'] = comments.count()
         return context
 
 
-
-
-# https://ccbv.co.uk/projects/Django/5.0/django.views.generic.edit/CreateView/
-
 class CommentCreateView(CreateView):
     model = Comment
-    fields = ['title', "content", 'rating',] # set the form fields here. 
+    fields = ['title', "content", 'rating',]  # set the form fields here
     template_name = 'blog/comment_form.html'
     success_url = "/"
 
-    # not working, needs looking into
+# not working, needs looking into
+
     def form_valid(self, form):
         wine_slug = self.kwargs.get('wine_slug')
         wine = get_object_or_404(Wine, slug=wine_slug)
         form.instance.wine = wine
-        form.instance.author = self.request.user # set form author to logged in user.
+        form.instance.author = self.request.user   # set form author to user.
         return super().form_valid(form)
-    
-    # redirect user to previous page. not working. 
+
+    # redirect user to previous page. not working.
     def get_success_url(self):
-        return reverse_lazy('wine_detail', kwargs={"slug": self.object.wine.slug})
+        return reverse_lazy(
+            'wine_detail',
+            kwargs={"slug": self.object.wine.slug}
+        )
 
 # comment edit/update view.
 # https://ccbv.co.uk/projects/Django/5.0/django.views.generic.edit/UpdateView/
 
-
-
-
 # ----- Editing reviews
+
 
 class EditCommentView(UpdateView):
     model = Comment
-    fields = ['title', 'content', 'rating']  # specify the fields you want in the form
+    fields = ['title', 'content', 'rating']  # specify the fields you want.
     template_name = 'blog/edit_comment.html'
     context_object_name = 'comment'
     success_url = "/"
@@ -75,15 +74,21 @@ class EditCommentView(UpdateView):
         return queryset.filter(author=self.request.user)
 
     def form_valid(self, form):
-        messages.success(self.request, 'Your comment has been updated and is under moderation.')
+        messages.success(
+            self.request,
+            'Your comment has been updated and is under moderation.'
+        )
         return super().form_valid(form)
 
     def get_success_url(self):
         # Redirect back to the wine detail page after editing
-        return reverse_lazy('wine_detail', kwargs={'slug': self.object.wine.slug})
+        return reverse_lazy(
+            'wine_detail', kwargs={'slug': self.object.wine.slug}
+        )
+
+# ----- Deleting comments
 
 
-    # ----- Deleting comments
 @login_required
 def delete_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
